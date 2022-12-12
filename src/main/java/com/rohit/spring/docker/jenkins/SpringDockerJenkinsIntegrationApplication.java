69f161527c9c46cdde231324aee9dd6837e5be23
rohitm53 <rohitmanohar53@gmail.com>
2022-12-12 19:31:48 +0530

    First commit

---
+++ src/main/java/com/rohit/spring/docker/jenkins/SpringDockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.spring.docker.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohit/spring/docker/jenkins/SpringDockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.spring.docker.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
